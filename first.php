bc05efdde395f035f818f49a147eef3cb8f3317d
Tuan Kiet <kiethdt.22ite@vku.udn.vn>
2023-08-17 22:44:43 +0700

    FD-install environments

---
+++ first.php
@@ -0,0 +1,14 @@
+<?php
+$customerName = 'DTKDEV-18';
+$customerAddress = 'Ngu Hanh Son';
+$customerAge = 18;
+echo $customerName;
+echo '<br/>';
+
+echo $customerAddress;
+
+echo '<br/>';
+
+echo $customerAge;
+
+var_dump($customerAge);
